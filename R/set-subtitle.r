a4682dabbab5bdeaf9c068805787b90d1adc3f61
Thomas Reinholdsson <reinholdsson@gmail.com>
2012-12-09 15:44:55 +0100

    Add set_title and set_subtitle

---
+++ R/set-subtitle.r
@@ -0,0 +1,38 @@
+#' Set subtitle
+#'
+#' Set a new subtitle for the chart
+#' @export
+#' @docType methods
+#' @rdname set_subtitle
+#'
+setGeneric("set_subtitle", function(object, 
+                                text = NULL,
+                                align = "center",
+                                floating = FALSE,
+                                margin = 15.0,
+                                style = NULL,
+                                useHTML = FALSE,
+                                verticalAlign = "top",
+                                x = 0.0,
+                                y = 30.0
+) {
+    standardGeneric("set_subtitle")
+})
+
+#' @rdname set_subtitle
+setMethod("set_subtitle", "chart", function(object, text, align, floating, margin, style, useHTML, verticalAlign, x, y) {
+    
+    object@options$subtitle <- list(
+        text = text,
+        align = align,
+        floating = floating,
+        margin = margin,
+        style = style,
+        useHTML = useHTML,
+        verticalAlign = verticalAlign,
+        x = x,
+        y = y
+    )
+    validObject(object)
+    return(object)
+}) 
