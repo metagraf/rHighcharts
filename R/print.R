c9dec3d39fcad8934da6ed92d8c1bc4b396c03b0
Thomas Reinholdsson <reinholdsson@gmail.com>
2012-12-09 15:57:10 +0100

    Test

--- R/print.R
+++ R/print.R
@@ -8,11 +8,6 @@
 #'
 setMethod("print", "chart", function(x) {
     
-    jquery_js <- file.path(system.file(package = "rHighcharts"), "jquery.min.js")
-    highcharts_js <- file.path(system.file(package = "rHighcharts"), "highcharts.js")
-    
-    html <- sprintf("\n<script type=\"text/javascript\">%s</script>\n\n<script type=\"text/javascript\">jQuery.noConflict();</script>\n\n<script type=\"text/javascript\">%s</script>\n\n<script type=\"text/javascript\">\n(function($){ $(function () { var chart = new Highcharts.Chart(%s);});})(jQuery);\n</script>\n\n<div id=\"highchart\"></div>", 
-        readChar(jquery_js, file.info(jquery_js)$size), readChar(highcharts_js, file.info(highcharts_js)$size), to_json(x))
     
     return(html)
 }) 
