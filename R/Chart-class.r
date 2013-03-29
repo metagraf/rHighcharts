c8b03c3dc459fd39994500085c25ea2287687b7b
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-03-29 09:24:48 +0100

    Update code

--- R/Chart-class.r
+++ R/Chart-class.r
@@ -93,9 +93,8 @@ Chart <- setRefClass(
         },
         
         show = function() {
-            assign(".chart_object", .self$copy(), envir = .GlobalEnv)  # put chart object in global variable (for shiny)
+            assign(.chart_object, .self$copy(), envir = .GlobalEnv)  # put chart object in global variable (for shiny)
             shiny::runApp(file.path(system.file(package = "rHighcharts"), "show-app"))
         }
-    ),
-    
+    )
 )
